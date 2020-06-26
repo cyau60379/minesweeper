953c0abf2bdb24175823dc9e341b67768271a01a
cyau60379 <cyril.aubourg@isep.fr>
2020-06-26 13:15:39 +0200

    Final version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='minesweeper',
+    version='1.2',
+    packages=['gui', 'logic'],
+    url='',
+    license='',
+    author='Cyril AUBOURG',
+    author_email='',
+    description='Minesweeper game'
+)
